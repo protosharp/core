9cdf60dac035f2f7a4f669e1b552b533cb0a8da0
sylviot <sylvio.tavares@hotmail.com>
2019-03-15 14:57:17 -0400

    Added SendText function on Context

--- Context.cs
+++ Context.cs
@@ -1,13 +1,16 @@
 using System.Net;
+using System.Text;
 
 namespace oopart
 {
     public class Context
     {
+        private Encoding encoding;
         public HttpListenerRequest Request {get; set;}
         public HttpListenerResponse Response {get; set;}
         public Context(HttpListenerRequest request, HttpListenerResponse response)
         {
+            this.encoding = Encoding.UTF8;
             this.Request = request;
             this.Response = response;
             this.Response.Headers["Server"] = "oopart";
@@ -17,11 +20,19 @@ namespace oopart
         {
             this.Response.ContentLength64 = bytes.Length;
             
-            var stream = Response.OutputStream;
+            var stream = this.Response.OutputStream;
             stream.Write(bytes, 0, bytes.Length);
             
             stream.Close();
             this.Response.Close();
+        }
+
+        public void SendText(string text)
+        { 
+            this.Response.Headers.Add("Content-Type", "text/plain");
+            
+            var bytes = this.encoding.GetBytes(text);
+            this.Send(bytes);
         }
     }
 }
