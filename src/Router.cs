5f76e89bcc2140c0cb3935c61a9ac5658bde1612
sylviot <sylvio.tavares@hotmail.com>
2019-03-16 01:16:24 -0400

    Call function from request

--- src/Router.cs
+++ src/Router.cs
@@ -1,10 +1,35 @@
+using System;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 
 namespace OOPart
 {
     public static class Router
     {
+        public static object Call(string functionName, string methodName, object[] parameters)
+        {
+            var types = Assembly.GetEntryAssembly().GetTypes().Concat(Assembly.GetExecutingAssembly().GetTypes());
+
+            foreach(var type in types)
+            {
+                if(type.Name.EndsWith(functionName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    foreach(var method in type.GetMethods())
+                    {
+                        if(method.Name.EndsWith(methodName, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            var instance = Activator.CreateInstance(type);
+                            
+                            return method.Invoke(instance, parameters);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
         public static string[] Parse(HttpListenerRequest request)
         {
             var splitted = request.Url.Segments
@@ -14,10 +39,18 @@ namespace OOPart
             
             if (IsFilename(request.Url.LocalPath))
             {
-                return new string[] {splitted.Last() };
+                return new [] {splitted.Last() };
             }
 
-            return new string[] { };
+            string controller = "Home", action = "Index";
+
+            if(splitted.Length >= 2)
+            {
+                controller = splitted[0];
+                action = splitted[1];
+            }
+
+            return new [] { controller, action }.Concat(splitted.Skip(2)).ToArray<string>();
         }
 
         private static bool IsFilename(string url)
