a487f5a9d3ebe4b91adfaa331de1e3cf9311f9a6
sylviot <sylvio.tavares@hotmail.com>
2019-03-25 16:29:10 -0400

    Add basic model

---
+++ src/Model.cs
@@ -0,0 +1,49 @@
+using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace OOPArt
+{
+    public class Model: IDisposable
+    {
+        protected const string CONNECTION_STRING = "";
+        private IDbConnection _connection;
+
+        public Model()
+        {
+            this._connection = new NpgsqlConnection (CONNECTION_STRING);
+
+            this._connection.Open();
+        }
+
+        public virtual string TableName() => "unknow";
+
+
+        public IEnumerable<object> All()
+        {
+            return this._connection.Query<object>($"SELECT * FROM {this.TableName()};");
+        }
+
+        public bool Create()
+        {
+            return this._connection.Execute($"INSERT INTO {this.TableName()} (name, age) VALUES ('{Guid.NewGuid().ToString()}', {(new Random()).Next(0, 100)});") > 0;
+        }
+        
+        public object Find()
+        {
+            return this._connection.Query<object>($"SELECT * FROM {this.TableName()} LIMIT 1");
+        }
+
+
+        public void Dispose()
+        {
+            if(this._connection.State == ConnectionState.Open)
+            {
+                this._connection.Close();
+            }
+        }
+
+    }
+}
